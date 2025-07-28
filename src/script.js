c01c202b76528d5d3191e45fb090e980fd32deaa
Matthew <mcissell5115@gmail.com>
2025-07-28 14:32:31 -0500

    deploy

---
+++ src/script.js
@@ -0,0 +1,8 @@
+function del() {
+   document.getElementById("hide").style.display = "none";
+   document.getElementById("show").style.display = "block";
+}
+function message() {
+  document.getElementById("hide").style.display = "none";
+  document.getElementById("text").style.display = "block";
+}
